0bbba68cdde172addcdafde397e69889e0719415
Damar Buana Murti <damarbuanamurti@gmail.com>
2024-12-06 16:43:18 +0700

    add Mammal.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Animal from "./Animal.js";
-import Mammal from "../Mammal.js";
+import Mammal from "./Mammal.js";
 
 const rabbit = new Animal("Rabbit", "Omnivore", 100, true, 8);
 const orangUtan = new Mammal("Otan", "Omnivore", 200, false, 100);
